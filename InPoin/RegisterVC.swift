8f4d79bd93175b7791adc0e6c9c5c25ac6863a3c
Dafturn Technology <dafturn@Dafturns-iMac.local>
2017-04-20 20:32:37 +0700

    register

---
+++ InPoin/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  InPoin
+//
+//  Created by Dafturn Technology on 4/20/17.
+//  Copyright © 2017 Dafturn Technology. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
